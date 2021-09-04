b66c810fd788ba2790378845a3831721bf96e1f1
Redouane <redouane343@yahoo.com>
2021-09-04 19:10:38 +0100

    Update eerror.h

--- lib/base/eerror.h
+++ lib/base/eerror.h
@@ -112,7 +112,7 @@ void DumpUnfreed();
 
 #define CHECKFORMAT __attribute__ ((__format__(__printf__, 1, 2)))
 
-extern Sigc::Connection logConnection;
+extern sigc::Connection logConnection;
 extern sigc::signal2<void, int, const std::string&> logOutput;
 extern int logOutputConsole;
 
